5c7625f72318851788dcf76acc1acd6614ec5aa1
Mel.R <mel.redureau@gmail.com>
2024-04-09 15:54:13 +0200

    :sparkles: Ajout model Association Ajout de la table association.go et modification de la table User afin de matcher les ajouts. Correction orthographique dans la table USER Adress --> Address

--- models/user.go
+++ models/user.go
@@ -6,11 +6,12 @@ import (
 
 type User struct {
 	gorm.Model
-	Name     string `gorm:"type:varchar(100);not null"`
-	Email    string `gorm:"type:varchar(100);unique_index;not null"`
-	Password string `gorm:"type:varchar(100);not null"`
-	Adress   *string
-	Roles    []Roles `gorm:"many2many:user_languages;"`
-	Annonce  []Annonce
-	Favorite []Annonce
+	Name          string `gorm:"type:varchar(100);not null"`
+	Email         string `gorm:"type:varchar(100);unique_index;not null"`
+	Password      string `gorm:"type:varchar(100);not null"`
+	Address       *string
+	AssociationID uint
+	Roles         []Roles `gorm:"many2many:user_languages;"`
+	Annonce       []Annonce
+	Favorite      []Annonce
 }
