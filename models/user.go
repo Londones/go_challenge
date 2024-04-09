836ce58652f37e384fc301db42673c2a40ba6637
Mel.R <mel.redureau@gmail.com>
2024-04-09 12:33:53 +0200

    Ajout de différentes tables dans le fichier user.go

--- models/user.go
+++ models/user.go
@@ -1,7 +1,8 @@
-package main
+package models
 
 import (
 	"github.com/jinzhu/gorm"
+	"time"
 )
 
 type User struct {
@@ -9,4 +10,26 @@ type User struct {
 	Name     string `gorm:"type:varchar(100);not null"`
 	Email    string `gorm:"type:varchar(100);unique_index;not null"`
 	Password string `gorm:"type:varchar(100);not null"`
+	Adress   *string
+	Roles    []Roles `gorm:"many2many:user_languages;"`
+	Annonce  []Annonce
+	Favorite []Annonce
+}
+
+type Annonce struct {
+	gorm.Model
+	UserID uint
+	Cats   []Cats `gorm:"foreignKey:CustomReferer"`
+}
+
+type Cats struct {
+	gorm.Model
+	Name      string `gorm:"type:varchar(100);not null"`
+	Color     string `gorm:"type:varchar(100);not null"`
+	BirthDate *time.Time
+}
+
+type Roles struct {
+	gorm.Model
+	Name string `gorm:"type:varchar(100);not null"`
 }
