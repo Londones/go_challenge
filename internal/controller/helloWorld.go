82d40bc59e6f5fd45cdda53a23608a78c83335c8
Mevin Redureau <mel.redureau@gmail.com>
2024-04-12 21:09:54 +0200

    Ajout de swagger sur la route /swagger/ Pour l'instant il n'est pas possible de passer des annotations et donc de remplir le swagger.

---
+++ internal/controller/helloWorld.go
@@ -0,0 +1,18 @@
+package controller
+
+// HelloWorld godoc
+// @Summary      Hi
+// @Description  THIS DOES NOT WORD
+// @Tags         hello
+// @Accept       json
+// @Produce      json
+// @Param        id   path      int  true  "Account ID"
+// @Success      200  {object}  model.Account
+// @Failure      400  {object}  httputil.HTTPError
+// @Failure      404  {object}  httputil.HTTPError
+// @Failure      500  {object}  httputil.HTTPError
+// @Router       / [get]
+
+func HelloWorld() {
+
+}
