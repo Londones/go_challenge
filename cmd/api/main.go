5a3c89e518916aba8fdfb6eeda66430714033d0c
Mevin Redureau <mel.redureau@gmail.com>
2024-05-10 14:52:04 +0200

    Commit the main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,13 +6,36 @@ import (
 	"go-challenge/internal/server"
 )
 
+//    @title            GO-challenge-PurrfectMatch
+//    @version        1.0
+//    @description    Swagger de PurrfectMatch
+//    @termsOfService    http://swagger.io/terms/
+
+//    @contact.name    API Support
+//    @contact.url    http://www.swagger.io/support
+//    @contact.email    support@swagger.io
+
+//    @license.name    Apache 2.0
+//    @license.url    http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host        localhost:8080
+// @BasePath    /
+
+// Pour lancer le swagger : swag init --parseDependency -d ./internal/server -g ../../cmd/api/main.go
+// puis supprimer les lignes
 func main() {
 
 	auth.NewAuth()
-	server := server.NewServer()
+	server, error := server.NewServer()
+	if error != nil {
+		panic(fmt.Sprintf("cannot create server: %s", error))
+	}
 
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(fmt.Sprintf("cannot start server: %s", err))
 	}
+
+	// print that the server is running
+	fmt.Printf("Server is running on port %s", server.Addr)
 }
