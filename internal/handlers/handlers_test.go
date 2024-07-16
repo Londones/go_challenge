82e2092130203f442af902a407fcd72ef3d30cab
Mevin Redureau <mel.redureau@gmail.com>
2024-07-16 13:11:27 +0200

    Definitions des fonction CRUD dans le handlers_test.go

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -2,20 +2,152 @@ package handlers
 
 import (
 	"go-challenge/internal/database"
-	"os"
+	"go-challenge/internal/utils"
 	"testing"
 )
 
-func TestMain(m *testing.M) {
+func TestHandlers(t *testing.T) {
 	s, err := database.TestDatabaseInit()
 	if err != nil {
 		return
 	}
+	utils.Logger("debug", "GO-Tests", "Database", "Test database created")
+
+	// Auth
+
+	// User
+	TestCreateUser(t)
+	TestUpdateUser(t)
+	TestDeleteUser(t)
+	TestGetUser(t)
+	TestGetAllUsers(t)
 
 	// Annonce
+	TestCreateAnnonce(t)
+	TestUpdateAnnonce(t)
+	TestDeleteAnnonce(t)
+	TestGetAnnonce(t)
+	TestGetAllAnnonces(t)
 
-	ret := m.Run()
+	// Association
+	TestCreateAssociation(t)
+	TestUpdateAssociation(t)
+	TestDeleteAssociation(t)
+	TestGetAssociation(t)
+	TestGetAllAssociations(t)
 
-	database.TestDatabaseDestroy(s.Db)
-	os.Exit(ret)
+	// Cat
+	TestCreateCat(t)
+	TestUpdateCat(t)
+	TestDeleteCat(t)
+	TestGetCat(t)
+	TestGetAllCats(t)
+
+	// Favorites
+	TestCreateFavorites(t)
+	TestGetFavorites(t)
+	TestGetAllFavorites(t)
+
+	// Race
+	TestCreateRace(t)
+	TestGetAllRaces(t)
+	TestUpdateRace(t)
+	TestDeleteRace(t)
+	TestGetRace(t)
+
+	// Rating
+	TestCreateRating(t)
+	TestUpdateRating(t)
+	TestGetRatings(t)
+	TestGetAllRatings(t)
+	TestDeleteRatings(t)
+
+	// Room
+
+	destroy, err := database.TestDatabaseDestroy(s.Db)
+	if err != nil {
+		return
+	}
+	utils.Logger("debug", "GO-Tests", "Database", destroy)
+	utils.Logger("debug", "GO-Tests", "General", "End of Tests")
 }
+
+// Auth
+
+// User
+func TestCreateUser(t *testing.T) {
+	type User1 struct {
+	}
+}
+
+func TestUpdateUser(t *testing.T) {}
+
+func TestDeleteUser(t *testing.T) {}
+
+func TestGetUser(t *testing.T) {}
+
+func TestGetAllUsers(t *testing.T) {}
+
+// Annonce
+func TestCreateAnnonce(t *testing.T) {}
+
+func TestUpdateAnnonce(t *testing.T) {}
+
+func TestDeleteAnnonce(t *testing.T) {}
+
+func TestGetAnnonce(t *testing.T) {}
+
+func TestGetAllAnnonces(t *testing.T) {}
+
+// Association
+func TestCreateAssociation(t *testing.T) {}
+
+func TestUpdateAssociation(t *testing.T) {}
+
+func TestDeleteAssociation(t *testing.T) {}
+
+func TestGetAssociation(t *testing.T) {}
+
+func TestGetAllAssociations(t *testing.T) {}
+
+// Cat
+func TestCreateCat(t *testing.T) {}
+
+func TestUpdateCat(t *testing.T) {}
+
+func TestDeleteCat(t *testing.T) {}
+
+func TestGetCat(t *testing.T) {}
+
+func TestGetAllCats(t *testing.T) {}
+
+// Favorites
+func TestCreateFavorites(t *testing.T) {}
+
+func TestGetFavorites(t *testing.T) {}
+
+func TestGetAllFavorites(t *testing.T) {}
+
+// Race
+func TestCreateRace(t *testing.T) {}
+
+func TestGetAllRaces(t *testing.T) {}
+
+func TestUpdateRace(t *testing.T) {}
+
+func TestDeleteRace(t *testing.T) {}
+
+func TestGetRace(t *testing.T) {}
+
+// Rating
+func TestCreateRating(t *testing.T) {}
+
+func TestUpdateRating(t *testing.T) {}
+
+func TestGetRatings(t *testing.T) {}
+
+func TestGetAllRatings(t *testing.T) {}
+
+func TestDeleteRatings(t *testing.T) {}
+
+// Rooms
